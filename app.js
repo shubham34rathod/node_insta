aa8a4d1eac3afb3ac693f1bd705e44f2819c5a2e
shubham34rathod <shubham34rathod@gmail.com>
2023-03-31 15:15:18 +0530

    insta node is added

--- app.js
+++ app.js
@@ -5,24 +5,20 @@ const multer=require("multer")
 const {GridFsStorage}=require("multer-gridfs-storage")
 const {GridFSBucket,MongoClient}=require("mongodb")
 require("dotenv").config()
-
 let cors=require("cors");
 const { Schema } = require("mongoose");
 const data = require("./db/sample.js");
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-
 const client=new MongoClient(process.env.DB_URL)
-
 app.get("/postdata/:file",async(req,res)=>{
-    console.log(req.file)
+    // console.log(req.file)
     try{
          await client.connect()
          const database=client.db(process.env.DATABASE)
          const photobucket=new GridFSBucket(database,
             {bucketName:process.env.PHOTOCOLLECTION})
-
             const event=photobucket.openDownloadStreamByName(req.params.file)
             event.on("data",(g)=>{
                 return res.write(g)
@@ -32,15 +28,12 @@ app.get("/postdata/:file",async(req,res)=>{
             })
             event.on("end",(g)=>{
                 return res.end()
-
             })
-
     }
     catch(e){
         res.send(e)
     }
 })
-
 const Storage=new GridFsStorage({
     url:process.env.DB_URL + process.env.DATABASE,
     file:(req,file)=>{
@@ -55,12 +48,9 @@ const Storage=new GridFsStorage({
 const upload=multer({
     storage:Storage
 })
-
-
 app.get("/",(req,res)=>{
     res.send("hello")
 })
-
 app.get("/postdata",async(req,res)=>{
     try{
   let data=await dbModel.find({})
@@ -70,32 +60,28 @@ app.get("/postdata",async(req,res)=>{
         res.send(e)
     }
 })
-
 app.post("/postdata",upload.single("PostImage"),async(req,res)=>{
     try{
-    //  console.log(req.body)
-    //  const createschema= await new dbModel({
-    //     name:req.body.name,
-    //     location:req.body.location,
-    //     likes:req.body.likes,
-    //     description:req.body.description,
-    //     PostImage:req.file.filename,
-    //     date:req.body.date
-
-    //  })
-     
-    //  const data= await createschema.save()
-    //  res.send(data)
-
-    // console.log(req.body);
-    console.log(req.body.name);
+     console.log(req.body)
+     const createschema= await new dbModel({
+        name:req.body.name,
+        location:req.body.location,
+        likes:req.body.likes,
+        description:req.body.description,
+        PostImage:req.file.filename,
+        date:req.body.date
+     })
+     const data= await createschema.save()
+     res.send(data)
+    console.log(req.body);
     }
     catch(e)
     {
+        console.log("sending error");
         res.send(e)
     }
 })
-
 app.listen(8000,()=>{
-    console.log("connected to port 8000")
+    console.log("run successfully")
 })
+
